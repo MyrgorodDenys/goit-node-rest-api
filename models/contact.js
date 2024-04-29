6433ffdd235fb116c03855d4d1e89266e32912c1
MyrgorodDenys <dmyrte@gmail.com>
2024-04-29 15:36:55 +0300

    29.04.2024.1

---
+++ models/contact.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { mongooseError } = require("../helpers/mongooseError");
+
+const phoneRegExp = /^\(\d{3}\) \d{3}-\d{4}$/;
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      match: phoneRegExp,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", mongooseError);
+
+const createContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  favorite: Joi.boolean(),
+});
+
+const schemas = {
+  createContactSchema,
+  updateContactSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,22 +1,36 @@
-import express from "express";
-import {
+const express = require("express");
+
+const {
   getAllContacts,
   getOneContact,
   deleteContact,
   createContact,
   updateContact,
-} from "../controllers/contactsControllers.js";
+  updateStatusContact,
+} = require("../controllers/contactsControllers");
+const { validateBody } = require("../helpers/validateBody");
+const { isValidId } = require("../helpers/isValidId");
+const {
+  schemas: { createContactSchema, updateContactSchema },
+} = require("../models/contact");
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", isValidId, getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", isValidId, deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(updateContactSchema),
+  updateContact
+);
 
-export default contactsRouter;
+contactsRouter.patch("/:id/favorite", isValidId, updateStatusContact);
+
+module.exports = contactsRouter;
