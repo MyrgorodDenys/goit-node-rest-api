eb335a426879b31671ee09a27bd7b59d06de36c9
MyrgorodDenys <dmyrte@gmail.com>
2024-05-25 22:56:20 +0300

    25.05.24.22.56

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,20 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+import Jimp from "jimp";
+import gravatar from "gravatar";
+import path from "node:path";
+import { fileURLToPath } from "url";
+import fs from "node:fs/promises";
+
 import { User } from "../models/user.js";
 import HttpError from "../helpers/HttpError.js";
 
 const { SECRET_KEY } = process.env;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const avatarsDir = path.join(__dirname, "..", "public", "avatars");
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -15,7 +25,12 @@ export const registerUser = async (req, res, next) => {
 
   try {
     const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+    const avatarURL = gravatar.url(email);
+    const newUser = await User.create({
+      ...req.body,
+      password: hashPassword,
+      avatarURL,
+    });
     res.status(201).json({
       email: newUser.email,
       subscription: newUser.subscription,
@@ -104,6 +119,38 @@ export const updateSubscription = async (req, res, next) => {
       throw HttpError(404, "Not authorized");
     }
     res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateAvatar = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      return next(HttpError(400, "No file uploaded"));
+    }
+
+    if (!user) {
+      return next(HttpError(401, "Not authorized"));
+    }
+
+    const { _id } = req.user;
+    const user = await User.findById(_id);
+    const { path: tempUpload, originalname } = req.file;
+    const fileName = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, fileName);
+
+    await fs.rename(tempUpload, resultUpload);
+    const image = await Jimp.read(resultUpload);
+    await image.resize(250, 250).writeAsync(resultUpload);
+
+    //const avatarURL = path.join(fileName);
+    const avatarURL = path.join("avatars", fileName);
+
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.status(200).json({
+      avatarURL,
+    });
   } catch (error) {
     next(error);
   }
